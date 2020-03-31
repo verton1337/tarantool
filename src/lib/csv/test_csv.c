13a2b9256884bf2831962d3755faf7e5c9a2cad9
Sergey Bronnikov <sergeyb@tarantool.org>
2020-03-31 14:44:01 +0000

    Add infrastructure for fuzzing testing and fuzzers
    There is a number of bugs related to parsing and encoding/decoding data.
    
    Examples:
    
    - msgpack: #5184, #5017, #5016, #5014, #4724, #3900
    - csv: #2692, #4497, #2692
    - uri: #585
    - json: #4339, #4366
    - xlog_meta_parse: #1928
    - iproto: #206
    - sql: ???
    
    One of the effective method to find such issues is a fuzzing testing.
    
    - introduce CMake flag to enable building fuzzers
    - add fuzzers based on LibFuzzer[1] to csv, http_parser and uri modules
    
    [1] https://llvm.org/docs/LibFuzzer.html
    
    Note: LibFuzzer requires Clang compiler
    
    How-To Use:
    
    $ mkdir build && cd build
    $ CC=clang CXX=clang++ cmake -DENABLE_FUZZER=ON ..
    $ make -j
    $ mkdir corpus
    
    Create data with valid examples to fuzzed functions in corpus/ dir
    and start fuzzing:
    
    $ ./Testing/test_csv corpus/ -max_total_time=60*60*60 -workers=4
    ...
    
    Follows #1809

---
+++ src/lib/csv/test_csv.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "csv.h"
+
+int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
+        struct csv csv;
+        csv_create(&csv);
+        char *buf = calloc(size, sizeof(char*));
+        if (buf == NULL)
+                return -1;
+        memcpy(buf, data, size);
+        buf[size] = '\0';
+        char *end = buf + size;
+        csv_parse_chunk(&csv, buf, end);
+        csv_finish_parsing(&csv);
+        csv_destroy(&csv);
+        free(buf);
+
+        return 0;
+}
