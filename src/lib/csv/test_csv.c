dd92f2ab894989fb12ed0b891b0f7b31ce6fd722
Sergey Bronnikov <sergeyb@tarantool.org>
2020-06-11 15:58:32 +0000

    csv check err code

--- src/lib/csv/test_csv.c
+++ src/lib/csv/test_csv.c
@@ -15,8 +15,9 @@ int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         char *end = buf + size;
         csv_parse_chunk(&csv, buf, end);
         csv_finish_parsing(&csv);
+        int rc = csv_get_error_status(&csv) == CSV_ER_INVALID ? 1 : 0;
         csv_destroy(&csv);
         free(buf);
 
-        return 0;
+        return rc;
 }
