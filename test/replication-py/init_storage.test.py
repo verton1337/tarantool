e20ec655a7dfb102a71680aeb14f48960e3869b5
Alexander V. Tikhonov <avtikhon@tarantool.org>
2020-11-08 01:03:19 +0300

    test: replication-py/init_storage.test.py
    Found that test failed in 2 common places when it tried to start the
    replica and wait it within 'JOIN' either 'SUBSCRIBE' test parts.
    It used to wait for replica start check the 'wait_until_started()'
    function 'TarantoolServer' class from test-run repository. But it
    didn't try resolve connection issues on replica creation, like:
    
      [30534] main/103/replica I> connecting to 1 replicas
      [30534] main/112/applier/localhost:49168 I> can't connect to master
      [30534] main/112/applier/localhost:49168 sio.c:208 !> SystemError connect to 127.0.0.1:49168, called on fd 27, aka 127.0.0.1:47954: Connection refused
      [30534] main/112/applier/localhost:49168 I> will retry every 0.10 second
      [30534] main/112/applier/localhost:49168 I> remote master c5d480c3-219c-11eb-ac14-080027727614 at 127.0.0.1:49168 running Tarantool 2.7.0
      [30534] main/103/replica I> connected to 1 replicas
      [30534] main/103/replica I> bootstrapping replica from c5d480c3-219c-11eb-ac14-080027727614 at 127.0.0.1:49168
      [30534] main/112/applier/localhost:49168 I> can't read row
      [30534] main/112/applier/localhost:49168 box.cc:183 E> ER_READONLY: Can't modify data because this instance is in read-only mode.
      [30534] main/103/replica box.cc:183 E> ER_READONLY: Can't modify data because this instance is in read-only mode.
      [30534] main/103/replica F> can't initialize storage: Can't modify data because this instance is in read-only mode.
      [30534] main/103/replica F> can't initialize storage: Can't modify data because this instance is in read-only mode.
    
    To resolve it the test was changed to be able to catch exception
    'TarantoolStartError' from test-run. Also the test should have the
    ability to be restarted by test-run using fragile list and in this way
    'crash_expected' flag was disabled to let the test fail with exception.
    
    Also found the new issues, like:
    
      Test hung! Result content mismatch:
      --- replication-py/init_storage.result  Wed Aug 26 06:06:15 2020
      +++ /tmp/tnt/101_replication-py/init_storage.result     Wed Nov 11 10:17:50 2020
      @@ -130,5 +130,3 @@
       -------------------------------------------------------------
       waiting reconnect on JOIN...
       ok
      -waiting reconnect on SUBSCRIBE...
      -ok
    
    to fix it decided to use SIGKILL signal in restart/stop routines to
    stop the instances.
    
    Needed by #4949

--- test/replication-py/init_storage.test.py
+++ test/replication-py/init_storage.test.py
@@ -1,5 +1,6 @@
 import os
 import glob
+from lib.tarantool_server import TarantoolStartError
 from lib.tarantool_server import TarantoolServer
 
 # master server
@@ -23,16 +24,16 @@ replica.rpl_master = master
 replica.deploy()
 replica.admin('box.space.test:select()')
 
-replica.restart()
+replica.restart(signal=9)
 replica.admin('box.space.test:select()')
-replica.stop()
+replica.stop(signal=9)
 replica.cleanup()
 
 print '-------------------------------------------------------------'
 print 'replica test 2 (must be ok)'
 print '-------------------------------------------------------------'
 
-master.restart()
+master.restart(signal=9)
 master.admin('for k = 10, 19 do box.space[42]:insert{k, k*k*k} end')
 master.admin("for k = 20, 29 do box.space[42]:upsert({k}, {}) end")
 lsn = master.get_lsn(master_id)
@@ -48,14 +49,14 @@ replica.wait_lsn(master_id, lsn)
 for i in range(1, 20):
     replica.admin('space:get{%d}' % i)
 
-replica.stop()
+replica.stop(signal=9)
 replica.cleanup()
 
 print '-------------------------------------------------------------'
 print 'reconnect on JOIN/SUBSCRIBE'
 print '-------------------------------------------------------------'
 
-server.stop()
+server.stop(signal=9)
 replica = TarantoolServer(server.ini)
 replica.script = 'replication-py/replica.lua'
 replica.vardir = server.vardir #os.path.join(server.vardir, 'replica')
@@ -64,20 +65,38 @@ replica.deploy(wait=False)
 
 print 'waiting reconnect on JOIN...'
 server.start()
-replica.wait_until_started()
-print 'ok'
+try:
+    # Replica may fail to start due connection issues may occur, check
+    # gh-4949. Also the test should have the ability to be restarted by
+    # test-run using fragile list and in this way 'crash_expected' flag
+    # should be disabled to let the test fail with exception.
+    replica.crash_expected = True
+    replica.wait_until_started()
+except TarantoolStartError:
+    print 'not ok - server failed to start'
+else:
+    print 'ok'
 
-replica.stop()
-server.stop()
+replica.stop(signal=9)
+server.stop(signal=9)
 
 print 'waiting reconnect on SUBSCRIBE...'
 replica.start(wait=False)
 server.start()
-replica.wait_until_started()
-print 'ok'
+try:
+    # Replica may fail to start due connection issues may occur, check
+    # gh-4949. Also the test should have the ability to be restarted by
+    # test-run using fragile list and in this way 'crash_expected' flag
+    # should be disabled to let the test fail with exception.
+    replica.crash_expected = True
+    replica.wait_until_started()
+except TarantoolStartError:
+    print 'not ok - server failed to start'
+else:
+    print 'ok'
 
-replica.stop()
+replica.stop(signal=9)
 replica.cleanup()
 
-server.stop()
+server.stop(signal=9)
 server.deploy()
